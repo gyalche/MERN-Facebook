dd750c0fa34da4dc113aed080c6cf1011f0e324c
Dawa <sherpadawa01@outlook.com>
2022-09-07 00:23:36 +0545

    working on dummy data

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './post.css';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-const Post = () => {
+const Post = ({ post }) => {
+  console.log(post);
   return (
     <div className='post'>
       <div className='postWrapper'>
@@ -9,24 +10,24 @@ const Post = () => {
           <div className='postTopLeft'>
             <img src='/assets/myPhoto.jpg' alt='' className='postProfileImg' />
             <span className='postUsername'>Dawa don</span>
-            <span className='postDate'>5 Min ago</span>
+            <span className='postDate'>{post?.date}</span>
           </div>
           <div className='postTopRight'>
             <MoreVertIcon />
           </div>
         </div>
         <div className='postCenter'>
-          <span className='postText'>It my first post</span>
-          <img src='assets/school.jpg' alt='' className='postImage' />
+          <span className='postText'>{post?.desc}</span>
+          <img src={post.photo} alt='' className='postImage' />
         </div>
         <div className='postBottom'>
           <div className='postBottomLeft'>
             <img src='assets/fblike.png' alt='' className='likeIcon' />
             <img src='assets/heart.png' alt='' className='likeIcon' />
-            <span className='postLikeCounter'>32 people liked</span>
+            <span className='postLikeCounter'>{post.like} people liked</span>
           </div>
           <div className='postBottomRight'>
-            <span className='postCommentText'>9 comments</span>
+            <span className='postCommentText'>{post.comment} comments</span>
           </div>
         </div>
       </div>
---
+++ frontend/src/dummyData.js
@@ -0,0 +1,46 @@
+export const Users = [
+  {
+    id: 1,
+    profilePicture: 'assets/mp.jpg',
+    username: 'Safak Kocaoglu',
+  },
+  {
+    id: 2,
+    profilePicture: 'assets/school.jpg',
+    username: 'Janell Shrum',
+  },
+  {
+    id: 3,
+    profilePicture: 'assets/myPhoto.jpg',
+    username: 'Alex Durden',
+  },
+];
+
+export const Posts = [
+  {
+    id: 1,
+    desc: 'Love For All, Hatred For None.',
+    photo: 'assets/my.jpg',
+    date: '5 mins ago',
+    userId: 1,
+    like: 32,
+    comment: 9,
+  },
+  {
+    id: 2,
+    photo: 'assets/myPhoto.jpg',
+    date: '15 mins ago',
+    userId: 2,
+    like: 2,
+    comment: 1,
+  },
+  {
+    id: 3,
+    desc: 'Every moment is a fresh beginning.',
+    photo: 'assets/school.jpg',
+    date: '1 hour ago',
+    userId: 3,
+    like: 61,
+    comment: 2,
+  },
+];
